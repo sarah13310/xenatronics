9bbb2df2220af851b62865b7660d4b4fc60c0eb3
sarah13310 <xenatronics@laposte.net>
2023-09-24 10:04:35 +0200

    Version 1.2 Form Login et register

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,24 +8,36 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+
+
+    private function loadmenu($file){
+        $people_json = file_get_contents($file);
+        $tab_json = json_decode($people_json, false);
+        return $tab_json;
+    }
+
+    #[Route('/', name: 'app.home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'menu' => $this->loadmenu("data/menu.json"),
         ]);
     }
+
     #[Route('/portfolio', name: 'portfolio')]
     public function portfolio(): Response
     {
         return $this->render('home/portfolio.html.twig', [
             'controller_name' => 'HomeController',
+            'menu' => $this->loadmenu("data/menu.json"),
         ]);
     }
     #[Route('/about', name: 'about')]
     public function about(): Response
     {
         return $this->render('home/about.html.twig', [
+            'menu' => $this->loadmenu("data/menu.json"),
+
             'action' => '/sendcontact',
         ]);
     }
---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    private function loadmenu($file){
+        $people_json = file_get_contents($file);
+        $tab_json = json_decode($people_json, false);
+        return $tab_json;
+    }
+
+    #[Route(path: '/login', name: 'app.login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+         return $this->redirectToRoute('app.home', ['auth'=> False]);
+        }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig',
+            ['last_username' => $lastUsername,
+                'error' => $error,
+                'menu' => $this->loadMenu("data/menu.json")
+            ]
+        );
+    }
+
+    #[Route(path: '/logout', name: 'app.logout')]
+    public function logout(): void
+    {
+    }
+}
